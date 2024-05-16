daab3ecd66dd4d367c4801fceedc00a7252b1416
Payd3r <payd3r2003@gmail.com>
2024-05-16 19:19:46 +0200

    e

---
+++ B/climatemonitoring/src/main/java/com/mycompany/climatemonitoring/Climatemonitoring.java
@@ -0,0 +1,12 @@
+package com.mycompany.climatemonitoring;
+
+/**
+ *
+ * @author Payd3r
+ */
+public class Climatemonitoring {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
